e411d5b58c1052e9ec5e603af41227a62c8cf044
Stefane M <stefane.maria@alu.ufc.br>
2022-05-17 10:41:18 -0300

    First class: Position

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardGame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Position pos = new Position(2, 5);
+		System.out.println(pos);
+		
+	}
+
+}
