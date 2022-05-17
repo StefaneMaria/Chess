3fbee69beffbe4f97e5d8640961e30f0dd5924f0
Stefane M <stefane.maria@alu.ufc.br>
2022-05-17 12:00:37 -0300

    Chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,14 @@
 package application;
 
-import boardGame.Board;
-import boardGame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Board board = new Board(8, 8);
+		ChessMatch chessMath = new ChessMatch();
+		
+		UI.printBoard(chessMath.getPieces());
 		
 	}
 
---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,46 @@
+package chess;
+
+import boardGame.Board;
+import boardGame.Piece;
+import boardGame.Position;
+
+public class ChessPiece extends Piece{
+	
+	private Color color;
+	private int   moveCount;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+//	a cor de uma peca nao pode ser modificada
+	public Color getColor() {
+		return color;
+	}
+
+	public int getMoveCount() {
+		return moveCount;
+	}
+
+	public void setMoveCount(int moveCount) {
+		this.moveCount = moveCount;
+	}
+	
+	public ChessPosition getChessPosition() {
+		return null;
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		return true;
+	}
+	
+	protected void increaseMoveCount() {
+		
+	}
+	
+	protected void decreaseMoveCount() {
+		
+	}
+
+}
