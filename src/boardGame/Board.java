24ff8b6f8b0aa902bbb2e8095528e43065dca8f0
Stefane M <stefane.maria@alu.ufc.br>
2022-05-18 08:33:15 -0300

    Moving pieces

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -54,9 +54,18 @@ public class Board {
 	
 	public Piece removePiece(Position position) {
 		
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
 		
-		
-		return null;
+		return aux;
+
 	}
 	
 	private boolean positionExists(int row, int column) {
