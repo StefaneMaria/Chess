58bcaa1c5fa6087baf22c2b37877baa6d1394ba3
Stefane M <stefane.maria@alu.ufc.br>
2022-05-19 13:06:58 -0300

    Check Logic

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -2,6 +2,8 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import boardGame.Board;
 import boardGame.Piece;
@@ -15,12 +17,12 @@ public class ChessMatch {
 	
 	private int turn;
 	private Color currentPlayer;
-	private boolean checkMate;
+	private boolean check;
 	
 //	private ChessPiece enPassantVulnerable;
 //	private ChessPiece promoted;
 	
-	private List<ChessPiece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
 	
 	public ChessMatch() {
@@ -36,6 +38,10 @@ public class ChessMatch {
 
 	public Color getCurrentPlayer() {
 		return currentPlayer;
+	}
+	
+	public boolean getCheck() {
+		return check;
 	}
 
 	public ChessPiece[][] getPieces(){
@@ -77,6 +83,38 @@ public class ChessMatch {
 		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
 	
+	private Color opponet(Color color) {
+		return (color == Color.WHITE) ? Color.BLACK : Color.WHITE;
+	}
+	
+	private ChessPiece king(Color color) {
+		
+		List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == color).collect(Collectors.toList());
+		for(Piece p : list) {
+			if(p instanceof King) {
+				return (ChessPiece)p;
+			}
+		}
+		throw new IllegalStateException("There is no " + color + "king on the board.");
+		
+	}
+
+	private boolean testCheck(Color color) {
+		
+		Position kingPosition = king(color).getChessPosition().toPosition();
+		List<Piece> opponentPieces = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == opponet(color)).collect(Collectors.toList());
+		
+		for(Piece p: opponentPieces) {
+			boolean[][]	mat = p.possibleMoves();
+			if(mat[kingPosition.getRow()][kingPosition.getColumn()]) {
+				return true;
+			}
+		}
+		
+		return false;
+		
+	}
+	
 //	serve pra imprimir as posicoes possiveis dada uma posicao inicial
 	public boolean[][] possibleMoves(ChessPosition sourcePosition){
 		
@@ -93,8 +131,15 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
-		nextTurn();
 		
+		if(testCheck(currentPlayer)) {
+			undoMove(source, target, capturedPiece);
+			throw new ChessException("You can't put yourself in check");
+		}
+		
+		check = (testCheck(opponet(currentPlayer))) ? true : false;
+		
+		nextTurn();
 		return (ChessPiece)capturedPiece;
 	}
 	
@@ -110,6 +155,19 @@ public class ChessMatch {
 		}
 		
 		return capturedPiece;
+		
+	}
+	
+	private void undoMove (Position source, Position target, Piece capturedPiece) {
+		
+		Piece p = board.removePiece(target);
+		board.placePiece(p, source);
+		
+		if(capturedPiece != null) {
+			board.placePiece(capturedPiece, target);
+			capturedPieces.remove(capturedPiece);
+			piecesOnTheBoard.add(capturedPiece);
+		}
 		
 	}
 	
